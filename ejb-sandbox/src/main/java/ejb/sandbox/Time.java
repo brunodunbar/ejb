c731cb59ea04c18e66913f9f058673e22460b177
brunodunbar <brunodunbar@gmail.com>
2014-06-03 23:29:32 -0300

    Adicionado exemplo do JAX-WS

---
+++ ejb-sandbox/src/main/java/ejb/sandbox/Time.java
@@ -0,0 +1,16 @@
+package ejb.sandbox;
+
+import javax.ejb.EJB;
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService
+public class Time {
+	@EJB
+	private TimeOfDay timeOfDay;
+
+	@WebMethod(operationName = "timeOfDay")
+	public String timeOfDay() {
+		return timeOfDay.timeOfDay();
+	}
+}
