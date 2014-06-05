ff27f85fb15ab12985d858e807efc55511595ef9
brunodunbar <brunodunbar@gmail.com>
2014-06-04 23:05:44 -0300

    Corrigindo Typo.

--- ejb-sandbox/src/main/java/ejb/sandbox/rest/AppConfig.java
+++ ejb-sandbox/src/main/java/ejb/sandbox/rest/AppConfig.java
@@ -5,7 +5,7 @@ import javax.ws.rs.core.Application;
 
 /**
  * 
- * Classe de configuração do JAX-RS, efitando assim que seja necessário ter um
+ * Classe de configuração do JAX-RS, evitando assim que seja necessário ter um
  * Servlet mapeado no web.xml
  * 
  * @author Bruno
